02fe0045651eb2afdd4f4203104fc29346f5756b
uttanutta <uttanutta@users.noreply.github.com>
2022-05-20 18:49:26 +0100

    Friday

--- src/App.js
+++ src/App.js
@@ -2,56 +2,18 @@ import './App.css';
 import React from 'react';
 import { cardData } from "./data";
 
-function Suits(props){
+function Interpretation(props){
   return(
     <div>
-    <label htmlFor="suit-select">Choose a suit:</label>
-    <select name="suits" id="suit-select" defaultValue={props.defaultSuit} onChange={props.handleSuitChange}>
-        <option value="Trumps" >Trumps</option>
-        <option value="Cups">Cups</option>
-        <option value="Wands">Wands</option>
-        <option value="Swords">Swords</option>
-        <option value="Pentacles">Pentacles</option>
-    </select>  
-  <p>Your current choice: {props.currentSuit}</p>  
-  </div>  
-  )
-}
-
-function FullDeck(props){
-  //Was {cardData.map((data, key) => {
-  return (
-      <div className="deck-container">
-        {props.deck.cardData.map((data, key) => {  
-          return (
-            <div key={key}>
-              {data.Title +
-                "; " +
-                data.Suit +
-                ";" +
-                data.Ordinal +
-                ";" +
-                data.OrdinalName }
-            </div>
-          );
-        })}
-      </div>
-  );
-}
-
-function Ordinals(props){
-  console.log(props);
-  return(
-    <div>
-    <label htmlFor="ordinal-select">Choose an ordinal:</label>
-    <select name="ordinals" id="ordinal-select">
-        {props.currentOrdinals.remaining.map((data, key) => {  
-          return (
-            <option key={key} value={data.Ordinal}>{data.OrdinalName}</option>
-          );
-        })}
-    </select>
-</div>  
+      <br/>
+        {<img src={(props.result[0]) ? props.result[0].Image : ""} height="250px"/>}
+        <p>Interpretation&nbsp;:&nbsp;
+        {(props.result[0]) ? props.result[0].Title : ""}
+        <br/>
+      </p>        
+      <p><strong>Normal</strong>&nbsp;: {(props.result[0]) ? props.result[0].Normal: ""}</p> 
+      <p><strong>Reversed</strong>&nbsp;: {(props.result[0]) ? props.result[0].Reversed : ""}</p>      
+    </div>  
   )
 }
 
@@ -60,32 +22,41 @@ class TarotApp extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      selectedSuit: "Swords",
-      deck: {cardData},
-      remainingCardsBySuit: 
-        {remaining : cardData.filter((el)=>{
-          return el.Suit == "Swords";
-        })},
+      selectedSuit: null,
+      deck: cardData,
+      remainingCardsBySuit: null,
+      selectedOrdinal: 10,
     }
   }
   
   getAvailableOrdinals(suit){
-    const changedArray=this.state.deck.cardData.filter((el) => {      
+    const changedArray=this.state.deck.filter((el) => {      
       return el.Suit==suit;
     });
     return{"remaining":changedArray}
   }
+
   handleSuitChange(e) {   
-    const newSuit =  e.target.value
-    console.log('newSuit: ' + newSuit);
-    this.setState({selectedSuit: newSuit,remainingCardsBySuit: this.getAvailableOrdinals(newSuit)});
-    console.log(this.state.remainingCardsBySuit);
+    const newSuit =  e.target.value;
+    this.setState({selectedSuit: newSuit,remainingCardsBySuit: this.getAvailableOrdinals(newSuit),selectedOrdinal: ""});
+  }  
+
+  handleOrdinalChange(e) {   
+    const newOrdinal =  e.target.value;
+    this.setState({selectedOrdinal: newOrdinal});
   }  
   render() {
     const currentSuit = this.state.selectedSuit
-    console.log('currentSuit: ',currentSuit);
+    const currentOrdinal = this.state.selectedOrdinal
     const filteredCards = this.state.remainingCardsBySuit
-    console.log('filteredCards: ',filteredCards);
+    const selectedOrdinal = this.state.selectedOrdinal
+    const filteredOrdinals = this.state.deck.filter(
+      (o) => o.Suit === currentSuit
+    );
+    console.log(this.state.deck[0].Normal)
+    const filteredCard = this.state.deck.filter(
+            (o) => o.Ordinal == selectedOrdinal && o.Suit == currentSuit
+    );
     return (
       <div className="App">
         <h1 className="App-header">
@@ -96,35 +67,63 @@ class TarotApp extends React.Component {
           defaultSuit={currentSuit}
           currentSuit={currentSuit}
         />  
-        <Ordinals
-          currentOrdinals={filteredCards}
-        />
-        <FullDeck
-          deck={this.state.deck}
-        />
-        <div>
-            <button className="none">
-              Pick
-            </button>
-        </div> 
-        <div>
-            <ol id="dealt-hand" visibile="hidden">
-                <li>King of Pentacles [pic, description]</li>
-                <li>XXI of Trumps, The World [pic , description]</li>
-                <li>XV of Trumps, The Devil [pic , description]</li>
-                <li>III of Cups  [pic , description]</li>
-                <li>Ace of Wands  [pic , description]</li>                    
-            </ol>
-        </div>
-        <div>
-            <button className="none">
-              Restart
-            </button>
-        </div>                               
+        <Ordinals 
+          options={filteredOrdinals}
+          handleOrdinalChange={(e) => this.handleOrdinalChange(e)}
+          selectedOrdinal={currentOrdinal}
+        /> 
+        <Interpretation
+          result={filteredCard}
+        />           
       </div>
 
     );
   }
 }
 
+function Suits(props){
+  //{props.defaultSuit}
+  return(
+    <div>
+    <label htmlFor="suit-select">Choose a suit: </label>
+    <select name="suits" id="suit-select" defaultValue="" onChange={props.handleSuitChange}>    
+        <option value="" >-- Select Suit --</option>
+        <option value="Trumps" >Trumps</option>
+        <option value="Cups">Cups</option>
+        <option value="Wands">Wands</option>
+        <option value="Swords">Swords</option>
+        <option value="Pentacles">Pentacles</option>
+    </select>  
+  
+  </div>  
+  )
+}
+
+function Ordinals(props){
+  return(
+    <div>
+    <label htmlFor="ordinal-select">Choose an ordinal:</label>
+    <select name="ordinals" id="ordinal-select" default="" onChange={props.handleOrdinalChange}>
+      <option value="" >-- Select Ordinal --</option>
+        {props.options.map((data, key) => {  
+          return (
+            <option key={data.Id} value={data.Ordinal}>{data.OrdinalName}</option>
+          );
+        })}
+    </select>    
+    </div>  
+  )
+}
+
+function Pick(props){
+  //console.log(props);
+  return(
+    <span>
+    <button className="pick" onClick={props.onClick}>
+    Pick
+  </button>
+  &nbsp;
+  </span>
+  )
+}
 export default TarotApp;
