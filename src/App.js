a3c56f6c0ebc868d12a38392e2f23c9d6c5f3a32
uttanutta <uttanutta@users.noreply.github.com>
2022-05-24 19:12:12 +0100

    Music

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import { cardData } from "./data";
+import { layout } from "./layout-data.js";
 
 function Interpretation(props){
   const genderedCard = props.result[0] && props.result[0].GenderedCard != "Non-specific";
@@ -31,17 +32,17 @@ function Interpretation(props){
             >Switch to reversed presentation
           </button>           
         </div>
-          {<img src={(props.result[0]) ? props.result[0].Image : ""}  id="cardImage" className="Normal"/>}
+          {<img src={(props.result[0]) ? props.result[0].Image : ""}  id="cardImage" className="Normal" />}
           <br/>     
           <div  id="Gendered" className="gendered">
             {genderedCard ? <span>This card also represents the {props.result[0].GenderedCard} gender.</span> : ""} 
           </div>          
-        <div  id="NormalInterpretation" className="pVisibleInline">
+        <div  id="NormalInterpretation" className="pVisibleInline" title="hoverin' words">
           Divinatory Meaning&nbsp;:&nbsp;
             {(props.result[0]) ? props.result[0].Title : ""}&nbsp;<strong>(Normal)</strong><br/>
             {(props.result[0]) ? props.result[0].Normal: ""}
         </div> 
-        <div  id="ReversedInterpretation" className="pHidden">
+        <div  id="ReversedInterpretation" className="pHidden" title="hoverin' words">
           Divinatory Meaning&nbsp;:&nbsp;
             {(props.result[0]) ? props.result[0].Title : ""}&nbsp;<strong>(Reversed)</strong><br/>
             {(props.result[0]) ? props.result[0].Reversed : ""}
@@ -59,6 +60,7 @@ class TarotApp extends React.Component {
   constructor(props){
     super(props)
     const deckCopy = cardData.map((x) => x);
+    const layoutCopy = layout.map((x) => x);
     this.state = {
       selectedSuit: null,
       deck: deckCopy,
@@ -66,6 +68,10 @@ class TarotApp extends React.Component {
       remainingCardsBySuit: null,
       selectedOrdinal: null,
       presentation: "Normal",
+      layout: layoutCopy,
+      nextLayoutPosition: layoutCopy.splice(0,1),
+      deckComplete:false,
+      pickedTitle:"You have picked no cards yet."
     }
   }
   
@@ -87,25 +93,49 @@ class TarotApp extends React.Component {
 
   handleReset(){
     const deckCopy = cardData.map((x) => x);
+    const layoutCopy = layout.map((x) => x);
     this.setState({
       deck: deckCopy,
-      picked: []
+      picked: [],
+      layout: layoutCopy,
+      nextLayoutPosition: layoutCopy.splice(0,1),
+      deckComplete:false,
+      pickedTitle:"You have picked no cards yet.",
     })
   }
   handleCardPicked(e){
     var index = this.state.deck.findIndex(x => x.Suit == this.state.selectedSuit && x.Ordinal == this.state.selectedOrdinal);
     var pickedCard=this.state.deck.splice(index,1)
     pickedCard[0].Presentation=document.getElementById('cardImage').className;
-    this.setState({
-      picked:this.state.picked.concat(pickedCard),
-    })
+    pickedCard[0].cardStatus=this.state.nextLayoutPosition[0].Title; 
+    console.log(this.state.nextLayoutPosition[0])   ;
+    pickedCard[0].cardStatusDescription=this.state.nextLayoutPosition[0].CrossMeaning;    
+    if(this.state.layout.length>0){    
+      this.setState({
+        nextLayoutPosition:this.state.layout.splice(0,1),
+        pickedTitle:"You have picked (so far):",
+        picked:this.state.picked.concat(pickedCard)
+    });      
+    }
+    else{
+      this.setState({
+        pickedTitle:"Your deck is complete:",
+        deckComplete:true,
+        picked:this.state.picked.concat(pickedCard)
+      })
+    }    
+    document.getElementById('Interpretation').className='pHidden'
+    document.getElementById('PickButton').className='pHidden'
     document.getElementById("ordinal-select").focus();
   }
 
   handleOrdinalChange(e) {   
-    const newOrdinal =  e.target.value;
-    this.setState({selectedOrdinal: newOrdinal});
-    {newOrdinal ? document.getElementById('Interpretation').className='pVisibleInline': document.getElementById('Interpretation').className='pHidden'}
+    if(!this.state.deckComplete){
+      const newOrdinal =  e.target.value;
+      this.setState({selectedOrdinal: newOrdinal});
+      {newOrdinal ? document.getElementById('Interpretation').className='pVisibleInline': document.getElementById('Interpretation').className='pHidden'}
+      document.getElementById('PickButton').className='pVisibleInline'
+    }
   }  
   render() {
     const currentSuit = this.state.selectedSuit
@@ -115,7 +145,6 @@ class TarotApp extends React.Component {
     const filteredOrdinals = this.state.deck.filter(
       (o) => o.Suit === currentSuit
     );
-    //console.log(this.state.deck[0].Normal)
     const filteredCard = this.state.deck.filter(
             (o) => o.Ordinal == selectedOrdinal && o.Suit == currentSuit
     );
@@ -124,6 +153,8 @@ class TarotApp extends React.Component {
         <h1 className="App-header">
           Tom's Tarot App
         </h1>
+        <p>{"Consider your question, and whom it concerns. Then choose 11 cards to complete a divination using \u2018The Cross\u2019"}</p>
+        <p>The next card <i>{this.state.nextLayoutPosition[0].Action}</i>.&nbsp;{this.state.nextLayoutPosition[0].CrossMeaning}</p>
         <Suits
           handleSuitChange={(e) => this.handleSuitChange(e)}
           defaultSuit={currentSuit}
@@ -139,9 +170,14 @@ class TarotApp extends React.Component {
         />  
         <Pick 
           pickedCards={this.state.picked}
+          pickedTitle={this.state.pickedTitle}
+          pickedDescription={this.state.pickedTitle}
           handleCardPicked={(e) => this.handleCardPicked(e)}
           handleReset={() => this.handleReset()}
-        />         
+        /> 
+        <p>Listen to music. It often helps:<br/>
+        <audio title="Neputune, The Mystic (Gustav Holst) - from The Internet Archive" src="https://ia803106.us.archive.org/22/items/lp_holst-the-planets_gustav-holst-leopold-stokowski-los-angeles/disc1/02.03.%20The%20Planets%3A%20Neptune%2C%20The%20Mystic.mp3" controls />                
+        </p>
       </div>
 
     );
@@ -149,7 +185,6 @@ class TarotApp extends React.Component {
 }
 
 function Suits(props){
-  //{props.defaultSuit}
   return(
     <div>
     <label htmlFor="suit-select">Choose a suit: </label>
@@ -185,21 +220,32 @@ function Ordinals(props){
 function Pick(props){
   return(
     <div id="Pick">
-      <button className="pick" onClick={props.handleCardPicked}>
-        Pick
-      </button>&nbsp;
+      <div id="PickButton" className="pHidden">      
+      <button onClick={props.handleCardPicked}>
+        Add Card to Deck
+      </button>
+      </div>
 
       <div>
-        <p>Your picks:</p>
+        <p>{props.pickedTitle}</p>        
             {props.pickedCards.map((data, key) => {  
               return (
-                <div key={data.Id}><strong>{data.Title},&nbsp;({data.Presentation}):</strong>&nbsp;{data.Presentation=="Normal"? data.Normal: data.Reversed}</div>
+                <div className={"floatedLeft "} key={data.Id} >                
+                <div className={"NarrowDiv30pc floatedLeft"} ><img src={data.Image} className={data.Presentation=="Normal"? "thumb Normal": "thumb Reversed"}/></div>
+                <div className={"NarrowDiv60pc floatedLeft"} >
+                  <strong title={data.cardStatusDescription}>{data.cardStatus}:</strong>&nbsp;
+                  <i>{data.Title},&nbsp;({data.Presentation}).</i>&nbsp;
+                  {data.Presentation=="Normal"? data.Normal: data.Reversed}
+                </div>
+                </div>
               );
             })}
-      </div>   
+      </div>  
+      <div className='clearFloat'>
       <button className="reset" onClick={props.handleReset}>
-        Reset
+        Reset the Deck
       </button>
+      </div> 
     </div>
   
   )
